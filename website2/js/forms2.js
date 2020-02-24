eb4230e617b97e6d3c2d2e27ca736fd95a7af03f
unknown <elina.postila@edu.keuda.fi>
2020-02-24 13:53:51 +0200

    24helmi

--- website2/js/forms2.js
+++ website2/js/forms2.js
@@ -1,42 +1,65 @@
-function validateForm() {
+function validateForm()
+{
   var nimi = document.nonsense.nimi;
-  if(nimi.value == "") {
+  if(nimi.value == "")
+  {
     alert("Nimi pitää olla!");
     nimi.focus();
     return false;
   }
   var ika = document.nonsense.ika;
-  if(ika.value == "") {
+  if(ika.value == "")
+  {
     alert("Ikä pitää olla!");
     ika.focus();
     return false;
   }
-  if(isNaN(ika.value)) {
+  if(isNaN(ika.value))
+  {
     alert("Vain numeroita ikä laatikkoon pls");
     ika.focus();
     return false;
   }
-  if(ika.value < 18) {
+  if(ika.value < 18)
+  {
     alert("Olet alaikäinen!");
     ika.focus();
     return false;
   }
   var nuts = document.nonsense.nuts;
-  if(nuts.value == "valitse") {
+  if(nuts.value == "valitse")
+  {
     alert("Valitse pähkinä!");
     nuts.focus();
     return false;
   }
+  var intVastaus=-1;
+
+  for (var i=0; i < document.nonsense.vari.length;i++)
+  {
+    if (document.nonsense.vari[i].checked==true)
+    {
+        intVastaus=i;
+    }
+  }
+  if(intVastaus==-1)
+  {
+  alert("Valitse YKSI väri");
+  return (false);
   }
 
-  /* MALLI
-  function checkCheckBoxes(theForm) {
-    if (
-    theForm.MyCheckbox.checked == false)
-    {
-        alert ('You didn\'t choose any of the checkboxes!');
-        return false;
-    } else {
-        return true;
-    }
-}*/
+var intVasta=-1;
+
+for (var i=0; i < document.nonsense.elain.length;i++)
+{
+  if (document.nonsense.elain[i].checked==true)
+  {
+      intVasta=i;
+  }
+}
+if(intVasta==-1)
+{
+alert("Valitse eläin");
+return (false);
+}
+}
